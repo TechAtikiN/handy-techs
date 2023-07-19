359f3771e9aa9e2e2b97ae494b4e13ebe482f33b
techatikin <nikitakhabya03@gmail.com>
2023-07-19 09:55:26 +0530

    feat: modified README.md, home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Handy Techs</h1>
-      <p>Handy Techs is a initiative to provide handy and useful techs. Explore more in the branches</p>
+    <main className='bg-slate-900 min-h-screen text-white px-10'>
+      <h1 className='text-4xl text-center font-bold py-5'>Handy Techs</h1>
+      <p
+        className='text-lg'
+      >
+        <strong>Handy Techs</strong> is a initiative to provide handy and useful techs. Explore more in the branches. Few of the techs that have been implemented are as follows:<br></br>
+        (Refer README.md of specific branch for more details)
+      </p>
     </main>
   )
 }

8ee00f2b66a98bdb692451f4fefde2a9a1abcf39
techatikin <nikitakhabya03@gmail.com>
2023-07-19 10:44:33 +0530

    download csv implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,65 @@
+'use client'
+import { useMemo, useState } from 'react'
+import { CSVLink } from 'react-csv'
+type Todo = {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
 export default function Home() {
+
+  const [data, setData] = useState<Todo[]>([])
+
+  const fetchData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5')
+    const data = await res.json()
+    setData(data)
+  }
+
+  const todos = useMemo(() => fetchData(), [])
+  const headers = [
+    { label: 'User Id', key: 'userId' },
+    { label: 'Id', key: 'id' },
+    { label: 'Title', key: 'title' },
+    { label: 'Completed', key: 'completed' },
+  ]
+
+  const csvReport = {
+    data: data,
+    headers: headers,
+    filename: 'data.csv'
+  }
+
   return (
     <main className='bg-slate-900 min-h-screen text-white px-10'>
-      <h1 className='text-4xl text-center font-bold py-5'>Handy Techs</h1>
-      <p
-        className='text-lg'
-      >
-        <strong>Handy Techs</strong> is a initiative to provide handy and useful techs. Explore more in the branches. Few of the techs that have been implemented are as follows:<br></br>
-        (Refer README.md of specific branch for more details)
-      </p>
+      <h1 className='text-2xl font-bold pt-5 pb-3'>Download Data</h1>
+      <p>Download CSV and JSON files in Next.js</p>
+      <table className='mt-5 bg-slate-800'>
+        <thead>
+          <tr>
+            <th className='px-4 py-3 border-2 font-semibold text-lg border-slate-500'>User Id</th>
+            <th className='px-4 py-3 border-2 font-semibold text-lg border-slate-500'>Id</th>
+            <th className='px-4 py-3 border-2 font-semibold text-lg border-slate-500'>Title</th>
+            <th className='px-4 py-3 border-2 font-semibold text-lg border-slate-500'>Completed</th>
+          </tr>
+        </thead>
+        <tbody className=''>
+          {data.map((todo, index) => (
+            <tr key={index} className='hover:bg-slate-950 hover:cursor-pointer'>
+              <td className='text-center px-3 py-2 border-2 border-slate-600'>{todo.userId}</td>
+              <td className='text-center px-3 py-2 border-2 border-slate-600'>{todo.id}</td>
+              <td className='text-center px-3 py-2 border-2 border-slate-600'>{todo.title}</td>
+              <td className='text-center px-3 py-2 border-2 border-slate-600'>{todo.completed ? 'True' : 'False'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button className='mt-5 px-3 py-2 rounded-full w-1/6 bg-white text-black font-semibold hover:bg-gray-200'>
+        <CSVLink {...csvReport}>Export to CSV</CSVLink>
+      </button>
     </main>
   )
 }

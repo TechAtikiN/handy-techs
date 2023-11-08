159bb05805497b1f92410f92460e2d021cf8b471
techatikin <nikitakhabya03@gmail.com>
2023-11-08 22:19:26 +0530

    feat: vercel edge setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,7 @@ export default function Home() {
   return (
     <main className='bg-slate-900 min-h-screen text-white px-10'>
       <h1 className='text-4xl text-center font-bold py-5'>Handy Techs</h1>
-      <p
-        className='text-lg'
-      >
-        <strong>Handy Techs</strong> is a initiative to provide handy and useful techs. Explore more in the branches. Few of the techs that have been implemented are as follows:<br></br>
-        (Refer README.md of specific branch for more details)
-      </p>
+      <p className='text-lg'>Vercel cron jobs</p>
     </main>
   )
 }
